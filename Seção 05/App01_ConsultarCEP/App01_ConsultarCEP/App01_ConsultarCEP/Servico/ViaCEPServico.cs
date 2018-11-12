342b531716bfe6aa8025280265626faea0a5d730
Guilherme Degrava <luigui_td@hotmail.com>
2018-11-12 12:43:27 -0200

    App01 - Concluído

---
+++ Seção 05/App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep); //Recebe a nova URL com o CEP desejado (o cep substitui {0} na URL)
+            WebClient wc = new WebClient();
+            string Conteudo = wc.DownloadString(NovoEnderecoURL); //Faz o download da informação da nova URL e armazena
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo); //Converte a informação de Conteudo e retorna um objeto do tipo Endereco
+
+            if(end.cep == null) return null;
+
+            return end; //Retorna a informação da Web convertida para o tipo Endereco
+        }
+    }
+}
